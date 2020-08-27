f904fa09c40b3fb52332af1cf1b27d4fd152f613
Natamo <madstk1@pm.me>
2020-08-27 16:34:38 +0200

    Change Example01 to show rotation, scaling and positioning

--- Examples/01-Core/Sandbox.cpp
+++ Examples/01-Core/Sandbox.cpp
@@ -9,11 +9,15 @@ public:
     Game(const ApplicationInfo &info) : Application(info) {}
 
     void Start() override {
-        Cube *cube1 = new Cube();
-        Cube *cube2 = new Cube();
+        for(u32 x = 0; x < 10; x++) {
+            for(u32 y = 0; y < 10; y++) {
+                Cube *cube = new Cube();
 
-        cube1->position = Vector3(0, 0, 0);
-        cube2->position = Vector3(3, 3, 3);
+                cube->position = Vector3(x * 2.0f, 0.0f, y * 2.0f);
+                cube->rotation = Vector3(6.f, 2.f, 5.f) * (f32)(x + y);
+                cube->scale = Vector3(2.0f / (x + y + 1));
+            }
+        }
     }
 
     void Update() override {
@@ -21,7 +25,6 @@ public:
 
     void FixedUpdate() override {
         float speed = 5.00f;
-        Logger::LogInfo("FPS: %f, memory: %ld bytes", Logger::GetFramesPerSecond(), Logger::GetMemoryAllocations());
 
         if(Input::GetKeyDown(Keycode::KeyW)) {
             g_Camera->GameObject()->position += g_Camera->Forward() * speed * Time::GetFixedDeltaTime();
