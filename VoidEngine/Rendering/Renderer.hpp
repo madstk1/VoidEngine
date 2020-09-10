35fccf2cd246810ac899edf3b82abbabaeff076b
Max <madstk1@pm.me>
2020-09-10 14:38:55 +0200

    Removed redundant default values.

--- VoidEngine/Rendering/Renderer.hpp
+++ VoidEngine/Rendering/Renderer.hpp
@@ -12,9 +12,9 @@ namespace VOID_NS {
     protected:
         static const u32 s_MaxTriangles = 100000;
 
-        i32             m_RefreshRate   = 144;
-        MultiSampling   m_Sampling      = MultiSampling::x4;
-        SwapInterval    m_Buffering     = SwapInterval::SingleBuffer;
+        i32             m_RefreshRate;
+        MultiSampling   m_Sampling;
+        SwapInterval    m_Buffering;
 
     public:
         Renderer(ApplicationInfo) {}
