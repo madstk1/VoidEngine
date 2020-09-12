1cac8e99ea527375a25f2a9462d0c468231c41b2
Max <madstk1@pm.me>
2020-09-12 18:23:00 +0200

    Added movement to FPS camera.

--- VoidEngine/ECS/Entities/FirstPersonCamera.hpp
+++ VoidEngine/ECS/Entities/FirstPersonCamera.hpp
@@ -10,6 +10,7 @@
 namespace VOID_NS {
     class FirstPersonCamera : public Camera {
     public:
+        f32 moveSpeed = 5.0f;
         f32 lookSensitivity = 1.0f;
 
         FirstPersonCamera() : Camera() {
@@ -21,6 +22,30 @@ namespace VOID_NS {
 
             if(rotation.y >  89.0f) { rotation.y =  89.0f; }
             if(rotation.y < -89.0f) { rotation.y = -89.0f; }
+
+            if(Input::GetKeyDown(Keycode::KeyW)) {
+                position += Forward() * moveSpeed * Time::GetDeltaTime();
+            }
+    
+            if(Input::GetKeyDown(Keycode::KeyS)) {
+                position -= Forward() * moveSpeed * Time::GetDeltaTime();
+            }
+    
+            if(Input::GetKeyDown(Keycode::KeyD)) {
+                position += Right() * moveSpeed * Time::GetDeltaTime();
+            }
+    
+            if(Input::GetKeyDown(Keycode::KeyA)) {
+                position -= Right() * moveSpeed * Time::GetDeltaTime();
+            }
+    
+            if(Input::GetKeyDown(Keycode::KeySpace)) {
+                position += Vector3(0, 1, 0) * moveSpeed * Time::GetDeltaTime();
+            }
+    
+            if(Input::GetKeyDown(Keycode::KeyC)) {
+                position -= Vector3(0, 1, 0) * moveSpeed * Time::GetDeltaTime();
+            }
         }
     };
 };
