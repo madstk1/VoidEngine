aae907fb5c2515095dc350def58b9a7dda54b799
Max <madstk1@pm.me>
2020-09-24 12:25:05 +0200

    Moved common types into namespace.

--- VoidEngine/Core/Common.hpp
+++ VoidEngine/Core/Common.hpp
@@ -17,32 +17,47 @@
 #define MAX(a,b)    (a > b ? a : b)
 #define MIN(a,b)    (a < b ? a : b)
 
-typedef uint64_t    u64;
-typedef  int64_t    i64;
+namespace VOID_NS {
+    using u64   = uint64_t;
+    using u32   = uint32_t;
+    using u16   = uint16_t;
+    using u8    = uint8_t;
+    using i64   = int64_t;
+    using i32   = int32_t;
+    using i16   = int16_t;
+    using i8    = int8_t;
+    using f64   = double;
+    using f32   = float;
+    using uchar = unsigned char;
 
-typedef uint32_t    u32;
-typedef  int32_t    i32;
+    using BufferID = u32;
+    using ShaderID = u32;
+    using Index = u32;
 
-typedef uint16_t    u16;
-typedef  int16_t    i16;
+    static const u64 U64_MAX = std::numeric_limits<u64>::max();
+    static const u64 U64_MIN = std::numeric_limits<u64>::min();
+    static const u32 U32_MAX = std::numeric_limits<u32>::max();
+    static const u32 U32_MIN = std::numeric_limits<u32>::min();
+    static const u16 U16_MAX = std::numeric_limits<u16>::max();
+    static const u16 U16_MIN = std::numeric_limits<u16>::min();
+    static const u8  U8_MAX  = std::numeric_limits<u8>::max();
+    static const u8  U8_MIN  = std::numeric_limits<u8>::min();
 
-typedef uint8_t     u8;
-typedef  int8_t     i8;
+    static const i64 I64_MAX = std::numeric_limits<i64>::max();
+    static const i64 I64_MIN = std::numeric_limits<i64>::min();
+    static const i32 I32_MAX = std::numeric_limits<i32>::max();
+    static const i32 I32_MIN = std::numeric_limits<i32>::min();
+    static const i16 I16_MAX = std::numeric_limits<i16>::max();
+    static const i16 I16_MIN = std::numeric_limits<i16>::min();
+    static const i8  I8_MAX  = std::numeric_limits<i8>::max();
+    static const i8  I8_MIN  = std::numeric_limits<i8>::min();
 
-typedef double      f64;
-typedef float       f32;
+    static const f64 F64_MAX = std::numeric_limits<f64>::max();
+    static const f64 F64_MIN = std::numeric_limits<f64>::min();
+    static const f32 F32_MAX = std::numeric_limits<f32>::max();
+    static const f32 F32_MIN = std::numeric_limits<f32>::min();
 
-typedef __u_char    uchar;
-
-/**
- * __bool_true_false_are_defined is
- * defined in stdbool.h/cstdbool, when bool is
- * already defined
- */
-#if ! defined(__cplusplus) && ! defined(__bool_true_false_are_defined)
-typedef uchar       bool;
-#define false       0
-#define true        1
-#endif /* __cplusplus && __bool_true_false_are_defined */
+    static const u32 InvalidID = U32_MAX;
+};
 
 #endif /* VOID_CORE_COMMON_H__ */
